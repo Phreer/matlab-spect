0fda62698d6d98d5251b31612aa6f76ef1b3e762
Phree Liu <iphreeliu@gmail.com>
2021-04-08 18:33:12 +0800

    Rename read_sino_norm_raw to read_tensor3d_norm_raw.

--- matlab/read_tensor3d_norm_raw.m
+++ matlab/read_tensor3d_norm_raw.m
@@ -1,11 +1,9 @@
-function [ Y ] = read_sinogram_norm_raw(filename, dtype, w, h, p)
+function [ Y ] = read_tensor3d_norm_raw(filename, dtype, w, h, p)
 %READ_SINOGRAM_NORM_A00 Read sinogram from raw data with data type and
 %shape (w, h, p).
-%   此处显示详细说明
 fid = fopen(filename, 'r');
 data = fread(fid, w * h * p, dtype);
 data = reshape(data, [w, h, p]);
 fclose(fid);
 Y = normalize_max(data);
 end
-
